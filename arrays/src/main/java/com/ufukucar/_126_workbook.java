bc3ea3f121f408e2df9f299fa84d3f053a7b185c
= <ufuk.ucar@tuseb.gov.tr>
2025-04-08 00:46:19 +0300

    =126 workbook yapildi

--- arrays/src/main/java/com/ufukucar/_126_workbook.java
+++ arrays/src/main/java/com/ufukucar/_126_workbook.java
@@ -2,8 +2,7 @@ package com.ufukucar;
 
 import java.util.Random;
 
-public class _125_workbook {
-
+public class _126_workbook {
 
     public static int randomNumber () {
         Random rand = new Random();
@@ -17,9 +16,9 @@ public class _125_workbook {
 
     public static void main(String[] args) {
 
-
         int [] sayi = new int[10];
         int highScore = 0;
+        int seat = 0;
 
         for (int i = 0; i < 10; i++) {
             sayi[i] = randomNumber();
@@ -30,13 +29,20 @@ public class _125_workbook {
             System.out.print(sayi[i] + " ");
         }
 
+
         for (int i = 0; i < sayi.length; i++) {
             if (sayi[i] > highScore) {
                 highScore = sayi[i];
+                seat = i;
             }
         }
 
         System.out.println("\nThe highest score is " + highScore + ". Impressive!");
+        System.out.println("It's the gentleman in seat: " + seat + ". Give that man a cookie!");
+
 
     }
+
+
+
 }
