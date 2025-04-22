6500deca17011eff0d75e94aa6d848a3b0be74b7
ufukucar <ufkucar@gmail.com>
2025-04-22 18:43:00 +0300

    Başladı 2

---
+++ higher-order-functions-and-lambda--streams/src/main/java/com/ufukucar/_277_WorkBook_13_1/Payment.java
@@ -0,0 +1,21 @@
+package com.ufukucar._277_WorkBook_13_1;
+
+import java.time.LocalDate;
+
+public class Payment {
+    private LocalDate date;
+    private double amount;
+
+    public Payment(LocalDate date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
