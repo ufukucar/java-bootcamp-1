58fa5b521f3e70fe2e590773016138d91ff0bc73
ufukucar <ufkucar@gmail.com>
2025-04-24 15:43:51 +0300

    Bitti

--- big-decimal/src/Main.java
+++ big-decimal/src/Main.java
@@ -1,15 +1,34 @@
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*
+        double x = 0.1;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(x);
+
+        double y = 0.2;
+        double z = x + y;
+
+        System.out.println(z);
+
+        /** ÇIKTI:
+         * 0.1
+         * 0.30000000000000004
+         */
+
+        BigDecimal x  = new BigDecimal("0.1");
+
+        System.out.println(x);
+
+        BigDecimal y = new BigDecimal("0.2");
+
+        System.out.println(x.add(y));
+        System.out.println(x.subtract(y));
+
+
+
     }
 }
