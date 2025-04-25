ff7ebd9526959a96d25a8003d47a9c15d704f9e0
ufukucar <ufkucar@gmail.com>
2025-04-25 16:54:08 +0300

    306 bitti. Son

--- capstone-project/src/Main.java
+++ capstone-project/src/Main.java
@@ -1,12 +1,18 @@
 import constants.TradeAccountType;
+import constants.Transaction;
 import pojo.CashAccount;
 import pojo.MarginAccount;
 import pojo.TradeAccount;
 import repository.TradeAccountRepository;
 import service.CashAccountService;
 import service.MarginAccountService;
+import service.TradeAccountService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
@@ -87,10 +93,66 @@ public class Main {
         marginAccountService.deleteTradeAccount("2");
         */
 
+        // PART 4
 
-
+        try {
+            loadTradeAccounts();
+            applyTransactions();
+            finalTest();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
 
     }
+
+
+
+    static Path[] paths = new Path[] {Paths.get("src/data/accounts.txt"), Paths.get("src/data/transactions.txt")};
+    static TradeAccountRepository tradeAccountRepository = new TradeAccountRepository();
+    static CashAccountService cashAccountService = new CashAccountService(tradeAccountRepository);
+    static MarginAccountService marginAccountService = new MarginAccountService(tradeAccountRepository);
+
+
+
+
+    public static void loadTradeAccounts() throws IOException {
+        Files.lines(paths[0])
+                .forEach(line -> {
+                    String[] words = line.split(" ");
+                    if (words[0].equals(TradeAccountType.CASH.toString())) {
+                        cashAccountService.createTradeAccount(new CashAccount(words[1], new BigDecimal(words[2])));
+                    } else {
+                        marginAccountService.createTradeAccount(new MarginAccount(words[1], new BigDecimal(words[2])));
+                    }
+                });
+    }
+
+    public static void applyTransactions() throws IOException {
+        Files.lines(paths[1])
+                .forEach(line -> {
+                    String[] words = line.split(" ");
+                    Boolean isCash = words[0].equals(TradeAccountType.CASH.toString());
+                    TradeAccountService tradeAccountService = isCash ? cashAccountService : marginAccountService;
+                    Transaction transaction = Transaction.valueOf(words[2]);
+                    if (transaction.equals(Transaction.DEPOSIT)) {
+                        tradeAccountService.deposit(words[1], new BigDecimal(words[3]));
+                    } else {
+                        tradeAccountService.withdraw(words[1], new BigDecimal(words[3]));
+                    }
+                });
+    }
+
+    public static void finalTest() throws IOException {
+        System.out.println("Account A1234B Cash Balance: " + ((CashAccount) cashAccountService.retrieveTradeAccount("A1234B")).getCashBalance());
+        System.out.println("Account E3456F Cash Balance: " + ((CashAccount) cashAccountService.retrieveTradeAccount("E3456F")).getCashBalance());
+        System.out.println("Account I5678J Cash Balance: " + ((CashAccount) cashAccountService.retrieveTradeAccount("I5678J")).getCashBalance());
+        System.out.println("Account C2345D Margin: " + ((MarginAccount) marginAccountService.retrieveTradeAccount("C2345D")).getMargin());
+        System.out.println("Account G4567H Margin: " + ((MarginAccount) marginAccountService.retrieveTradeAccount("G4567H")).getMargin());
+    }
+
+
+
+
 
 
 }
